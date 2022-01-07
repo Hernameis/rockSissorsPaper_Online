0a79605e5a8e28cd53c3aec19023ab9586c480a7
Hernameis <msh4287@naver.com>
2022-01-07 18:33:54 +0900

    socket opened

---
+++ TankTank/src/com/tanktank/server/ServerMain.java
@@ -0,0 +1,44 @@
+package com.tanktank.server;
+
+import java.io.*;
+import java.net.*;
+import java.io.IOException;
+
+public class ServerMain extends Thread{
+	Socket sock;
+	InputStream is;
+	OutputStream os;
+
+	public ServerMain(Socket socket){
+		sock=socket;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(sock.getInetAddress().getHostAddress()+"클라이언트 run중");
+	}
+	
+	public static void main(String[] args) {
+		ServerSocket serv=null;
+		Socket socket=null;
+		try {
+			serv=new ServerSocket(5000);
+			while(true){
+				socket=serv.accept();
+				Thread a=new ServerMain(socket);				
+				a.start();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
